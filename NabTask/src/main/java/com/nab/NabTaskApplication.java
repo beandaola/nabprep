05155a6ed9236104ef35c4edd3d0014751621ca5
beanliao <beanliao@beanliao.lan>
2019-03-04 14:31:32 +1100

    init project

---
+++ NabTask/src/main/java/com/nab/NabTaskApplication.java
@@ -0,0 +1,13 @@
+package com.nab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NabTaskApplication.class, args);
+	}
+
+}
